1ad812778550d7753012d2f6224bcabf4ce71149
lucasberenger <thegreatberenger@gmail.com>
2025-02-12 18:04:46 -0300

    feat: create user response dto

---
+++ src/main/java/com/chipnews/api/dtos/UserResponse.java
@@ -0,0 +1,52 @@
+package com.chipnews.api.dtos;
+
+import com.chipnews.api.entities.User;
+import org.springframework.beans.BeanUtils;
+
+public class UserResponse {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    // TODO: once the Package entity is created, add missing field.
+
+    public UserResponse(User user) {
+        BeanUtils.copyProperties(user, this);
+    }
+
+    public UserResponse() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
