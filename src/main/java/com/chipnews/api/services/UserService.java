bbd99390a5c155b7b2225dc8c8462af43bdc3ed5
lucasberenger <thegreatberenger@gmail.com>
2025-02-12 19:53:52 -0300

    feat: create getUserById service

--- src/main/java/com/chipnews/api/services/UserService.java
+++ src/main/java/com/chipnews/api/services/UserService.java
@@ -1,11 +1,13 @@
 package com.chipnews.api.services;
 
 import com.chipnews.api.dtos.UserResponse;
+import com.chipnews.api.entities.User;
 import com.chipnews.api.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -18,5 +20,11 @@ public class UserService {
                 .stream()
                 .map(UserResponse::new)
                 .toList();
+    }
+
+    public UserResponse getUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.map(UserResponse::new)
+                .orElseThrow(() -> new RuntimeException("User not found"));
     }
 }
