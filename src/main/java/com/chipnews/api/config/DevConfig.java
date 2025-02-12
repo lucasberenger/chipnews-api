d93f88db72c1b021efa1ee2de9f2e438e59dbbe1
lucasberenger <thegreatberenger@gmail.com>
2025-02-12 20:15:26 -0300

    test: create user in DevConfig file to test delete method

--- src/main/java/com/chipnews/api/config/DevConfig.java
+++ src/main/java/com/chipnews/api/config/DevConfig.java
@@ -1,5 +1,8 @@
 package com.chipnews.api.config;
 
+import com.chipnews.api.entities.User;
+import com.chipnews.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,9 +11,18 @@ import org.springframework.context.annotation.Profile;
 @Profile("dev")
 public class DevConfig implements CommandLineRunner {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public void run(String... args) throws Exception {
         System.out.println("DEV CONFIG IS RUNNING...");
 
+        // Creates a new user
+        User user1 = new User(null, "Do Da Da", "dodada@io.com", "123456", "123456789");
+
+        userRepository.save(user1);
     }
+
+
 }
