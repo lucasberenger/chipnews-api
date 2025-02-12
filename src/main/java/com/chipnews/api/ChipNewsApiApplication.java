492402ac8f064431c47ff540ea211b09b8dbf343
lucasberenger <thegreatberenger@gmail.com>
2025-02-12 17:20:07 -0300

    initial commit: create project

---
+++ src/main/java/com/chipnews/api/ChipNewsApiApplication.java
@@ -0,0 +1,13 @@
+package com.chipnews.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChipNewsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChipNewsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chipnews/api/ChipNewsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chipnews.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChipNewsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
