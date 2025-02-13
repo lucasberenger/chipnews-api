6105cc882161650058f65a39022bf7b7e4026376
lucasberenger <thegreatberenger@gmail.com>
2025-02-13 16:53:10 -0300

    feat: create address to User entity

--- src/main/java/com/chipnews/api/config/DevConfig.java
+++ src/main/java/com/chipnews/api/config/DevConfig.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.util.Arrays;
+
 @Configuration
 @Profile("dev")
 public class DevConfig implements CommandLineRunner {
@@ -19,9 +21,10 @@ public class DevConfig implements CommandLineRunner {
         System.out.println("DEV CONFIG IS RUNNING...");
 
         // Creates a new user
-        User user1 = new User(null, "Do Da Da", "dodada@io.com", "123456", "123456789");
+        User user1 = new User(null, "Do Da Da", "dodada@io.com", "123456789", "Avenue Av - 123","topSecret");
+        User user2 = new User(null, "Lucas", "lucas@io.com", "219988776655", "Street St - 425", "secretPassword");
 
-        userRepository.save(user1);
+        userRepository.saveAll(Arrays.asList(user1, user2));
     }
 
 
--- src/main/java/com/chipnews/api/entities/User.java
+++ src/main/java/com/chipnews/api/entities/User.java
@@ -16,20 +16,22 @@ public class User implements Serializable {
 
     private String name;
     private String email;
-    private String password;
     private String phone;
+    private String address;
+    private String password;
     // TODO: once the Package entity is created, add the relationship here!!
 
 
     public User() {
     }
 
-    public User(Long id, String name, String email, String password, String phone) {
+    public User(Long id, String name, String email, String phone, String address, String password) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.password = password;
         this.phone = phone;
+        this.address = address;
+        this.password = password;
     }
 
     public Long getId() {
@@ -70,6 +72,14 @@ public class User implements Serializable {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     @Override
